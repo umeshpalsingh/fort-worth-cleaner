e0052611bf174c46fa8da8cd73ef4257033bf182
unknown <umesh@ogrelogic.com>
2022-12-16 19:34:01 +0530

    7:33 PM code update

--- assets/js/custom_js.js
+++ assets/js/custom_js.js
@@ -29,9 +29,23 @@ function topFunction() {
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 66,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    }
+  }
 });     

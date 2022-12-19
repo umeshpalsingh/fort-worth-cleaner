0f63b848129a341c5f5528761b53eee12298cd4c
unknown <umesh@ogrelogic.com>
2022-12-19 17:21:00 +0530

    code update 5:20 PM 19 Dec

--- assets/js/custom_js.js
+++ assets/js/custom_js.js
@@ -50,3 +50,26 @@ var swiper = new Swiper(".mySwiper", {
     }
   }
 });     
+
+var swiper = new Swiper(".myFirstSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 66,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    }
+  }
+});   
